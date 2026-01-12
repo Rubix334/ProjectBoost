254e60ff66434ed581ce7cd1d9db576ae8f3e27e
Rubix334 <2260334@flourbluffschools.org>
2026-01-12 12:31:16 -0600

    Level 4 - Moving obstacle & script

--- project-boost/Player/player.gd
+++ project-boost/Player/player.gd
@@ -4,20 +4,25 @@ class_name Player
 @export_range(750,2500) var force := 1000.0
 @export var TorqueThrust := 100
 
+var transitioning := false
+
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("boost"):
-		apply_central_force(basis.y * delta * force)
-	if Input.is_action_pressed("rotateL"):
-		apply_torque(Vector3(0.0,0.0,delta*TorqueThrust))
-	if Input.is_action_pressed("rotateR"):
-		apply_torque(Vector3(0,0,-delta*TorqueThrust))
+	if not transitioning:
+		if Input.is_action_pressed("boost"):
+			apply_central_force(basis.y * delta * force)
+		if Input.is_action_pressed("rotateL"):
+			apply_torque(Vector3(0.0,0.0,delta*TorqueThrust))
+		if Input.is_action_pressed("rotateR"):
+			apply_torque(Vector3(0,0,-delta*TorqueThrust))
 
 func crash_sequence() -> void:
+	transitioning = true
 	print("KABOOM")
 	await get_tree().create_timer(2.5).timeout
 	get_tree().reload_current_scene.call_deferred()
 
 func complete_level(next_level_file) -> void:
+	transitioning = true
 	await get_tree().create_timer(2.5).timeout
 	get_tree().change_scene_to_file.call_deferred(next_level_file)
 
