9e3d9b7f88a5c98d2f1f62153ee53f563bc1fd77
Rubix334 <2260334@flourbluffschools.org>
2026-01-13 12:14:33 -0600

    Boosters and completed audio

--- project-boost/Player/player.gd
+++ project-boost/Player/player.gd
@@ -5,41 +5,64 @@ class_name Player
 @export var TorqueThrust := 100
 
 var transitioning := false
-var rocket_audio = $RocketAudio
+@onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+@onready var success_audio: AudioStreamPlayer3D = $SuccessAudio
+@onready var explosion_audio: AudioStreamPlayer3D = $ExplosionAudio
+@onready var main_booster: GPUParticles3D = $MainBooster
+@onready var right_booster: GPUParticles3D = $RightBooster
+@onready var left_booster: GPUParticles3D = $LeftBooster
+
 
 func _process(delta: float) -> void:
 	if not transitioning:
 		if Input.is_action_pressed("boost"):
 			apply_central_force(basis.y * delta * force)
+			main_booster.emitting = true
 			if not rocket_audio.is_playing():
 				rocket_audio.play()
 		else:
+			main_booster.emitting = false
 			rocket_audio.stop()
 		if Input.is_action_pressed("rotateL"):
 			apply_torque(Vector3(0.0,0.0,delta*TorqueThrust))
+			right_booster.emitting = true
+		else:
+			right_booster.emitting = false
 		if Input.is_action_pressed("rotateR"):
 			apply_torque(Vector3(0,0,-delta*TorqueThrust))
+			left_booster.emitting = true
+		else:
+			left_booster.emitting = false
 
 func crash_sequence() -> void:
 	transitioning = true
-	$ExplosionAudio.play()
+	explosion_audio.play()
+	rocket_audio.stop()
+	main_booster.emitting = false
+	right_booster.emitting = false
+	left_booster.emitting = false
 	print("KABOOM")
 	await get_tree().create_timer(2.5).timeout
 	get_tree().reload_current_scene.call_deferred()
 
 func complete_level(next_level_file) -> void:
 	transitioning = true
-	$SuccessAudio.play()
+	success_audio.play()
+	rocket_audio.stop()
+	main_booster.emitting = false
+	right_booster.emitting = false
+	left_booster.emitting = false
 	await get_tree().create_timer(2.5).timeout
 	get_tree().change_scene_to_file.call_deferred(next_level_file)
 
 func _on_body_entered(body: Node) -> void:
 	## Detects if the rockets hits any object in these groups and prints accordingly
-	if "Goal" in body.get_groups():
-		print("You Win!")
-		if body.file_path:
-			complete_level(body.file_path)
-		else:
-			print("No next level found!")
-	if "Hazard" in body.get_groups():
-		crash_sequence()
+	if not transitioning:
+		if "Goal" in body.get_groups():
+			print("You Win!")
+			if body.file_path:
+				complete_level(body.file_path)
+			else:
+				print("No next level found!")
+		if "Hazard" in body.get_groups():
+			crash_sequence()
